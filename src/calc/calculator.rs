8905591ad5344ada5797a68fa345873e2af3771d
VincentPT <minhpta@outlook.com>
2024-03-08 17:48:52 +0700

    ok

--- src/calc/calculator.rs
+++ src/calc/calculator.rs
@@ -1,64 +1,18 @@
-use std::rc::{Rc, Weak};
-
-pub trait CalculatorView {
-    fn set_result(self: &mut Self, result: String);
-    fn set_history(self: &mut Self, history: String);
-}
-pub struct Calculator<'a, TView> {
-    pub value: f64,
-    pub view: &'a mut TView,
+pub struct Calculator {
+    pub count: u32,
 }
 
 
-impl<'a, TView> Calculator<'a, TView> {
-    pub fn new(view: &'a mut TView) -> Self {
+impl Calculator {
+    pub fn new() -> Self {
         Self {
-            value: 0.0,
-            view,
+            count: 0
         }
     }
-}
 
-impl<'a, TView> Calculator<'a, TView> where TView:CalculatorView, {
-    pub fn do_something(self: &mut Self) {
-        self.view.set_result("Hello".to_string());
-    }
-}
-
-pub struct TestCalculator<TView> {
-    pub value: f64,
-    pub view: Rc<TView>,
-}
-
-
-impl<TView> TestCalculator<TView> {
-    pub fn new(view: Rc<TView>) -> Self {
-        Self {
-            value: 0.0,
-            view,
-        }
-    }
-}
-
-impl<TView> TestCalculator<TView> where TView:CalculatorView, {
-    pub fn do_something(&mut self) {
-        let view = Rc::get_mut(&mut self.view).unwrap();
-        view.set_result("Hello".to_string());    
-    }
-}
-
-
-pub struct TestCalculator2<TView> {
-    pub value: f64,
-    pub view: Weak<TView>,
-}
-
-
-impl<TView> TestCalculator2<TView> {
-    pub fn new(view: &Weak<TView>) -> Self {
-        Self {
-            value: 0.0,
-            view : view.clone(),
-        }
+    pub fn push_input(&mut self, input: char) -> (Option<String>, Option<String>) {
+        self.count += 1;
+        let history = self.count.to_string();
+        (Some(history), Some(input.to_string()))
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 mod calc;
 
+use std::cell::RefCell;
 use std::rc::Rc;
 
 use calc::calculator::Calculator;
-use calc::calculator::CalculatorView;
 
-use calc::calculator::TestCalculator;
-use calc::calculator::TestCalculator2;
 use druid::{
     theme, AppLauncher, Color, Data, Lens, LocalizedString, RenderContext, Widget, WidgetExt,
     WindowDesc,
@@ -15,38 +13,14 @@ use druid::{
 use druid::widget::{CrossAxisAlignment, Flex, Label, Painter};
 
 #[derive(Clone, Data, Lens)]
-struct CalcView {
-    /// The number displayed. Generally a valid float.
-    value: String,
+struct AppData {
     history: String,
+    value: String,
+    caculator: Rc<RefCell<Calculator>>,
 }
 
-impl CalculatorView for CalcView {
-    fn set_result(self: &mut Self, result: String) {
-        self.value = result;
-    }
 
-    fn set_history(self: &mut Self, history: String) {
-        self.history = history;
-    }
-}
-
-// impl Data for Calculator<'a, CalcView> {
-//     fn same(&self, _: &Self) -> bool {
-//         false
-//     }
-// }
-
-// impl Clone for Calculator<'a, CalcView> {
-//     fn clone(&self) -> Self {
-//         Self {
-//             value: self.value,
-//             view: self.view,
-//         }
-//     }
-// }
-
-impl CalcView {
+impl AppData {
     fn digit(&mut self, digit: u8) {
     }
 
@@ -57,10 +31,21 @@ impl CalcView {
     }
 
     fn op(&mut self, op: char) {
+        let mut caculator = self.caculator.borrow_mut();
+        let state = caculator.push_input(op);
+        match state.0 {
+            Some(history) => self.history = history,
+            None => {}
+        }
+        match state.1 {
+            Some(value) => self.value = value,
+            None => {}
+            
+        }
     }
 }
 
-fn op_button_label(op: char, label: String) -> impl Widget<CalcView> {
+fn op_button_label(op: char, label: String) -> impl Widget<AppData> {
     let painter = Painter::new(|ctx, _, env| {
         let bounds = ctx.size().to_rect();
 
@@ -80,14 +65,14 @@ fn op_button_label(op: char, label: String) -> impl Widget<CalcView> {
         .center()
         .background(painter)
         .expand()
-        .on_click(move |_ctx, data: &mut CalcView, _env| data.op(op))
+        .on_click(move |_ctx, data: &mut AppData, _env| data.op(op))
 }
 
-fn op_button(op: char) -> impl Widget<CalcView> {
+fn op_button(op: char) -> impl Widget<AppData> {
     op_button_label(op, op.to_string())
 }
 
-fn digit_button(digit: u8) -> impl Widget<CalcView> {
+fn digit_button(digit: u8) -> impl Widget<AppData> {
     let painter = Painter::new(|ctx, _, env| {
         let bounds = ctx.size().to_rect();
 
@@ -107,7 +92,7 @@ fn digit_button(digit: u8) -> impl Widget<CalcView> {
         .center()
         .background(painter)
         .expand()
-        .on_click(move |_ctx, data: &mut CalcView, _env| data.digit(digit))
+        .on_click(move |_ctx, data: &mut AppData, _env| data.digit(digit))
 }
 
 fn flex_row<T: Data>(
@@ -115,6 +100,7 @@ fn flex_row<T: Data>(
     w2: impl Widget<T> + 'static,
     w3: impl Widget<T> + 'static,
     w4: impl Widget<T> + 'static,
+    w5: impl Widget<T> + 'static,
 ) -> impl Widget<T> {
     Flex::row()
         .with_flex_child(w1, 1.0)
@@ -124,33 +110,54 @@ fn flex_row<T: Data>(
         .with_flex_child(w3, 1.0)
         .with_spacer(1.0)
         .with_flex_child(w4, 1.0)
+        .with_spacer(1.0)
+        .with_flex_child(w5, 1.0)
 }
 
-fn build_calc() -> impl Widget<CalcView> {
-    let display = Label::new(|data: &String, _env: &_| data.clone())
-        .with_text_size(32.0)
-        .lens(CalcView::value)
+fn build_calc() -> impl Widget<AppData> {
+    let lb_history = Label::new(|data: &String, _env: &_| data.clone())
+        .with_text_size(14.0)
+        .lens(AppData::history)
+        .padding(5.0);
+    let lb_result = Label::new(|data: &String, _env: &_| data.clone())
+        .with_text_size(14.0)
+        .lens(AppData::value)
         .padding(5.0);
     Flex::column()
         .with_flex_spacer(0.2)
-        .with_child(display)
+        .with_child(lb_history)
+        .with_flex_spacer(0.2)
+        .with_child(lb_result)
         .with_flex_spacer(0.2)
         .cross_axis_alignment(CrossAxisAlignment::End)
         .with_flex_child(
             flex_row(
                 op_button_label('c', "CE".to_string()),
-                op_button('C'),
+                op_button('C'),                
+                op_button_label('s', "MS".to_string()),
+                op_button_label('s', "MR".to_string()),
                 op_button('⌫'),
-                op_button('÷'),
             ),
             1.0,
         )
         .with_spacer(1.0)
         .with_flex_child(
             flex_row(
+                op_button_label('c', "sin".to_string()),
+                op_button_label('c', "cos".to_string()),
+                op_button_label('c', "tan".to_string()),
+                op_button_label('c', "⅟x".to_string()),
+                op_button('÷'), //
+            ),
+            1.0,
+        )
+        .with_spacer(1.0)        
+        .with_flex_child(
+            flex_row(
                 digit_button(7),
                 digit_button(8),
                 digit_button(9),
+                op_button_label('i', "x²".to_string()),
                 op_button('×'),
             ),
             1.0,
@@ -161,6 +168,7 @@ fn build_calc() -> impl Widget<CalcView> {
                 digit_button(4),
                 digit_button(5),
                 digit_button(6),
+                op_button_label('i', "√".to_string()),
                 op_button('−'),
             ),
             1.0,
@@ -171,6 +179,7 @@ fn build_calc() -> impl Widget<CalcView> {
                 digit_button(1),
                 digit_button(2),
                 digit_button(3),
+                op_button_label('i', "ln".to_string()),
                 op_button('+'),
             ),
             1.0,
@@ -178,55 +187,32 @@ fn build_calc() -> impl Widget<CalcView> {
         .with_spacer(1.0)
         .with_flex_child(
             flex_row(
-                op_button('±'),
                 digit_button(0),
                 op_button('.'),
+                op_button('e'),
+                op_button_label('i', "π".to_string()),
                 op_button('='),
             ),
             1.0,
         )
 }
 
-struct Test {
-    /// The number displayed. Generally a valid float.
-    value: String,
-    history: String,
-    caclulator: TestCalculator2<Test>,
-}
-
-impl Test {
-    fn new() -> Rc<Self> {
-        Rc::new_cyclic(|me| {
-            // Create the actual struct here.
-            Test { 
-                value: "0".to_string(),
-                history: String::new(),
-                caclulator: TestCalculator2::new(me),
-            }
-        })
-    }
-}
-
 pub fn main() {
     let window = WindowDesc::new(build_calc())
-        .window_size((223., 300.))
+        .window_size((403., 400.))
         .resizable(false)
         .title(
             LocalizedString::new("calc-demo-window-title").with_placeholder("Simple Calculator"),
         );
 
-
-    let test = Test::new();
-
-    let calc_state: CalcView = CalcView {
-        value: "0".to_string(),
+    let app_data: AppData = AppData {
         history: String::new(),
+        value: "0".to_string(),
+        caculator: Rc::new(RefCell::new(Calculator::new()))
     };
 
     AppLauncher::with_window(window)
         .log_to_console()
-        .launch(calc_state)
+        .launch(app_data)
         .expect("launch failed");
-
-
 }
